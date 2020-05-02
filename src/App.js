1c4edffa40440e1227e84d1d610920f48609078b
motokikasai <kmomoky@hotmail.com>
2020-05-02 12:12:01 +0200

    Add conditional switch to congrats

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <h3>Sign up</h3>
-        <form className="input-fields">
-          <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" />
-          <label htmlFor="password">Password</label>
-          <input type="text" name="password" id="password" />
-          <p>ERROR MSG</p>
-          <button>Sign up</button>
-        </form>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      success: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {!this.state.success ? (
+          <div className="main">
+            <div className="container">
+              <h3>Sign up</h3>
+              <form className="input-fields">
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" id="username" />
+                <label htmlFor="password">Password</label>
+                <input type="text" name="password" id="password" />
+                <p>ERROR MSG</p>
+                <button>Sign up</button>
+              </form>
+            </div>
+          </div>
+        ) : (
+          <div className="congrats">
+            <h1>Congrats! You made it!!</h1>
+            <h3>Welcome to the club</h3>
+            <p>Enjoy your stay</p>
+          </div>
+        )}
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
