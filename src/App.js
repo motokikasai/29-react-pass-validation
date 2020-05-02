5ff7d952071b4e046328472a484c6714b5bccd58
motokikasai <kmomoky@hotmail.com>
2020-05-02 12:00:31 +0200

    Set up basic structure with form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h3>Sign up</h3>
+        <form className="input-fields">
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" />
+          <label htmlFor="password">Password</label>
+          <input type="text" name="password" id="password" />
+          <p>ERROR MSG</p>
+          <button>Sign up</button>
+        </form>
+      </div>
     </div>
   );
 }
